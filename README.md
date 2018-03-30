79fafda38b897e3ee8f8cdbc3225d8aaf13006f1
Sean Baier <sean.t.baier@gmail.com>
2018-03-30 01:48:22 -0500

    Tightying up files

---
+++ README.md
@@ -0,0 +1,35 @@
+# Simple To-Do List using [Create React App](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#folder-structure)
+
+This app is already bootstrapped using Create React App, but if you'd like to
+start from scratch it can be found
+[here!](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md#folder-structure)
+as well as React's docs here ->
+[Reactjs.org](https://reactjs.org/docs/hello-world.html)
+
+## Get Started
+
+### 1. Make sure you have [Node](https://nodejs.org/en/download/package-manager/)
+
+installed
+
+1.  Clone this repository `git clone https://github.com/seantbaier/react-todo-list.git` or [download the
+    zip](https://github.com/seantbaier/react-todo-list/archive/master.zip)
+
+    _Your project folder should now have two subdirectories_
+
+    ```txt
+    todo-list/
+        README.md
+        .gitignore
+        todo-list-starter-files/
+        todo-list-final/
+    ```
+
+1.  Make sure you're in the `todo-list-starter-files` directory you just created.
+    `-cd todo-list-starter-files`
+
+1.  Install Node packages `npm install` or `yarn`
+1.  run the app `npm start` or `yarn start`
+1.  Install [React Developer
+    Tools](https://chrome.google.com/webstore/detail/react-developer-tools/fmkadmapgofadopljbjfkapdkoienihi?hl=en)
+    in your browser
--- todo-list-starter-files/src/components/App.js
+++ todo-list-starter-files/src/components/App.js
@@ -2,120 +2,18 @@ import React, { Component } from "react";
 import "../styles/reset.css";
 import "../styles/App.css";
 
-// Import components
-import List from "./List";
-import InputForm from "./InputForm";
-import Header from "./Header";
-
 class App extends Component {
   constructor(props) {
     super(props);
 
     // getInitialState
-    this.state = {
-      list: [
-        {
-          name: "Inventive Acaedmy Logo Design",
-          id: 0,
-          isEditing: false
-        },
-        {
-          name: "Daily Standup",
-          id: 1,
-          isEditing: false
-        },
-        {
-          name: "Reschedule lunch meeting",
-          id: 2,
-          isEditing: false
-        },
-        {
-          name: "Read emails",
-          id: 3,
-          isEditing: false
-        }
-      ],
-      pendingItem: ""
-    };
+    this.state = {};
   }
-
-  lastItemId = 3;
-
-  newItemId = () => {
-    const id = this.lastItemId;
-    this.lastItemId += 1;
-    return id;
-  };
-
-  // Flips isEditing bool
-  toggleIsEditingAt = id => {
-    console.log("isEditingAt", id);
-    this.setState({
-      list: this.state.list.map(item => {
-        if (id === item.id) {
-          return {
-            ...item,
-            isEditing: !item["isEditing"]
-          };
-        }
-        return item;
-      })
-    });
-  };
-
-  removeItemAt = id => {
-    this.setState({ list: this.state.list.filter(guest => id !== guest.id) });
-  };
-
-  handleItemInput = e => this.setState({ pendingItem: e.target.value });
-
-  // handle editing items
-  setNameAt = (name, id) => {
-    this.setState({
-      list: this.state.list.map(item => {
-        if (id === item.id) {
-          return {
-            ...item,
-            name
-          };
-        }
-        return item;
-      })
-    });
-  };
-
-  newItemSubmitHandler = e => {
-    e.preventDefault();
-    const id = this.newItemId();
-    this.setState({
-      list: [
-        {
-          name: this.state.pendingItem,
-          isEditing: false,
-          id
-        },
-        ...this.state.list
-      ],
-      pendingItem: ""
-    });
-  };
 
   render() {
     return (
       <div className="wrapper">
-        <Header />
-        <InputForm
-          newItemSubmitHandler={this.newItemSubmitHandler}
-          handleItemInput={this.handleItemInput}
-          pendingItem={this.state.pendingItem}
-        />
-
-        <List
-          list={this.state.list}
-          removeItemAt={this.removeItemAt}
-          toggleIsEditingAt={this.toggleIsEditingAt}
-          setNameAt={this.setNameAt}
-        />
+        <p>Let's make a todo list with React!</p>
       </div>
     );
   }
